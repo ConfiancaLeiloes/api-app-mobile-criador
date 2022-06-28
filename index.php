bea80118a51b7c994f9a1386b6c4338ebb5e5783
mtb-iago <iagooliveira09@outlook.com>
2022-06-28 14:58:21 -0300

    revisao de versao

--- index.php
+++ index.php
@@ -15,13 +15,31 @@ header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-W
 require_once 'loads.php';
 
 $app = AppFactory::create();
+$app->add(function (Request $request, RequestHandlerInterface $handler): Response {
+    $routeContext = RouteContext::fromRequest($request);
+    $routingResults = $routeContext->getRoutingResults();
+    $methods = $routingResults->getAllowedMethods();
+    $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
+
+    $response = $handler->handle($request);
+
+    $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+    $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
+    $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
+
+    return $response;
+});
+
+// The RoutingMiddleware should be added after our CORS middleware so routing is performed first
+$app->addRoutingMiddleware();
+
 $app->addErrorMiddleware(true, true, true);
 
 $app->setBasePath((function () {
     $scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
     $uri = (string) parse_url('http://a' . $_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
     if (stripos($uri, $_SERVER['SCRIPT_NAME']) === 0) {
-        return $_SERVER['SCRIPT_NAME'];
+        return $_SERVER['SCRIPT_NAME'].$uri;
     }
     if ($scriptDir !== '/' && stripos($uri, $scriptDir) === 0) {
         return $scriptDir;
@@ -29,9 +47,9 @@ $app->setBasePath((function () {
     return '';
 })());
 
+$app->get('/detalhes-animal-cobricoes/{id}', PlantelController::class . ':detalhes_animal_cobricoes');
 
-// The RoutingMiddleware should be added after our CORS middleware so routing is performed first
-$app->get('/', PlantelController::class . ':index');
+# ROTAS DE ANIMAIS
 
 try {
     $app->run();
--- plantel/app/controller/plantel.controller.php
+++ plantel/app/controller/plantel.controller.php
@@ -11,10 +11,35 @@ class PlantelController
 
     public function index(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
+
+        var_dump($request->getQueryParams());
+
+        exit;
+        //$plantel = new PlantelModel();
+        $resultado = $this->plantel->index();
+        $response->getBody()->write($resultado);
+        return $response->withStatus(200)->withHeader('Content-type', 'application/json');
+       
+    }
+    public function teste(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+
+        var_dump("teste rota 3");
+
+        exit;
+        //$plantel = new PlantelModel();
+        $resultado = $this->plantel->index();
+        $response->getBody()->write($resultado);
+        return $response->withStatus(200)->withHeader('Content-type', 'application/json');
+       
+    }
+    public function detalhes_animal_cobricoes(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
         //$plantel = new PlantelModel();
 
-
-        $resultado = $this->plantel->index();
+        // var_dump($request->getUri());
+        // return "foi";
+        $resultado = $this->plantel->detalhes_animal_cobricoes($request);
         $response->getBody()->write($resultado);
         return $response->withStatus(200)->withHeader('Content-type', 'application/json');
        
