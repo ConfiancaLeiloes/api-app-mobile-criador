2fb4bf0af50663739b41ed828132b8c28a625ed0
mtb-iago <iagooliveira09@outlook.com>
2022-07-20 17:33:15 -0300

    nova listagem para pendências de comunicacao

--- modulos/animais/controller/reproducao.controller.php
+++ modulos/animais/controller/reproducao.controller.php
@@ -58,4 +58,11 @@ class ReproducaoController
         return $response->withStatus(json_decode($resultado)->http_status_code)->withHeader('Content-type', 'application/json');
        
     }
+    public function listar_pendencias_coberturas(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $resultado = $this->reproducao->listar_pendencias_coberturas($request);
+        $response->getBody()->write($resultado);
+        return $response->withStatus(json_decode($resultado)->http_status_code)->withHeader('Content-type', 'application/json');
+       
+    }
 }
--- modulos/animais/model/reproducao.model.php
+++ modulos/animais/model/reproducao.model.php
@@ -898,4 +898,93 @@ class ReproducaoModel
         }
         
     }
+
+    public function listar_pendencias_coberturas(ServerRequestInterface $request)
+    {
+        $params = (array)$request->getParsedBody();
+        $id_proprietario     =  @$params['id_proprietario'];
+
+        if (!@$id_proprietario) 
+        return erro("Parâmetros inválidos ou faltantes!");
+        
+        try {
+            $query_sql = 
+                        "SELECT  
+                        tab_cobricoes.id_cobricao as ID_COBRICAO, 
+                        DATE_FORMAT(tab_cobricoes.data_cobertura, '%d/%m/%Y') as DATA_COBRICAO, 
+                        DATEDIFF(NOW(), tab_cobricoes.data_cobertura) AS NUMERO_DIAS_COBRICAO,
+                        CONCAT(tab_garanhao.nome, ' x ', tab_doadora.nome) as GARANHAO_DOADORA_COBRICAO, 
+                        IF(tab_cobricoes.id_te = '18', DATE_FORMAT(tab_cobricoes.data_te, '%d/%m/%Y'), '-') as DATA_TE_COBRICAO, 
+                        IF(ISNULL(tab_receptora.nome),'-',CONCAT(tab_receptora.marca, ' - ', tab_receptora.nome)) as NOME_RECEPTORA_COBRICAO, 
+                        (CASE WHEN tab_toques.id_situacao_prenhez IS NULL THEN 'SEM TOQUE' ELSE UPPER(tab_situacao_toque.descricao) END) as TOQUE_COBRICAO, 
+                        (CASE WHEN tab_toques.id_situacao_prenhez IS NULL THEN '00/00/0000' ELSE DATE_FORMAT(tab_toques.data_toque,'%d/%m/%Y') END) as DATA_TOQUE_COBRICAO, 
+                        IF((tab_toques.id_situacao_prenhez = '19' AND ISNULL(tab_nascimentos.id_nascimento)),CONCAT(DATEDIFF(CURDATE(), tab_cobricoes.data_cobertura), ' Dia(s)'),CONCAT('Nasceu: ',UPPER(tab_nascimentos.nome),' ',IF(tab_nascimentos.id_sexo = '2','(M)','(F)'))) as DIAS_GESTACAO_COBRICAO,
+                        tab_central.nome_razao_social as NOME_CENTRAL_COBRICAO, 
+                        tab_tipos_cobricoes.descricao as TIPO_COBRICAO,
+                        (
+                            CASE 
+                                WHEN tab_toques.id_situacao_prenhez IS NULL OR tab_toques.id_situacao_prenhez = '21' THEN '1' -- Sem Toque
+                                WHEN tab_toques.id_situacao_prenhez = '19' THEN '2' -- Positivo
+                                WHEN tab_toques.id_situacao_prenhez = '20' THEN '3' -- Negativo
+                            END
+                        ) as ID_TIPO_TOQUE_COBRICAO, 
+                        UPPER(tab_situacoes.descricao) as SITUACAO_COBRICAO, 
+                        IF(ISNULL(tab_situacao_sexagens.descricao),'SEM SEXAGEM',UPPER(tab_situacao_sexagens.descricao)) as SEXAGEM_COBRICAO, 
+                        IF(ISNULL(tab_comunicacoes_cobricao_associacao.protocolo_comunicacao),'SEM COMUNICAÇÃO',CONCAT(tab_comunicacoes_cobricao_associacao.protocolo_comunicacao,IF((NOT tab_comunicacoes_cobricao_associacao.id_receptora_comunicacao = tab_cobricoes.id_animal_receptora AND NOT tab_comunicacoes_cobricao_associacao.id_receptora_comunicacao IS NULL),CONCAT(' - ', UPPER(tab_receptora_comunicacao.nome)),''))) as PROTOCOLO_COMUNICACAO_COBRICAO,
+                        IF(ISNULL(tab_nascimentos.id_nascimento),'SEM NASCIMENTO',CONCAT(UPPER(tab_nascimentos.nome),' ',IF(tab_nascimentos.id_sexo = '2','(M)','(F)'))) as NASCIMENTO_COBRICAO,
+                        tab_toques.id_situacao_prenhez AS ID_SITUACAO_PRENHEZ,
+                        (
+                            CASE 
+                                WHEN tab_toques.id_situacao_prenhez = '19' THEN '2' -- Positivo
+                            END
+                        ) as TIPO_SITUACAO_PRENHEZ,
+                        (
+                            CASE 
+                                WHEN NOT tab_nascimentos.id_situacao_nascimento IS NULL THEN '2' -- nascidos
+                                WHEN tab_nascimentos.id_situacao_nascimento IS NULL THEN '3' -- Há nascer
+                            END
+                        ) as SITUACAO_NASCIMENTO,
+                        (
+                            CASE 
+                                WHEN tab_cobricoes.id_te = '17' THEN '2' -- Gestação Natural
+                                WHEN tab_cobricoes.id_te = '18' THEN '3' -- TE
+                            END
+                        ) as ID_TIPO_GESTACAO
+                    FROM tab_cobricoes  
+                        JOIN tab_animais AS tab_garanhao ON tab_garanhao.id_animal = tab_cobricoes.id_animal_macho  
+                        JOIN tab_animais AS tab_doadora ON tab_doadora.id_animal = tab_cobricoes.id_animal_femea  
+                        LEFT JOIN tab_animais AS tab_receptora ON tab_receptora.id_animal = tab_cobricoes.id_animal_receptora
+                        JOIN tab_tipos_cobricoes ON tab_tipos_cobricoes.id_tipo_cobricao = tab_cobricoes.id_tipo_cobricao
+                        JOIN tab_pessoas AS tab_central ON tab_central.id_pessoa = tab_cobricoes.id_central_reproducao   
+                        JOIN tab_situacoes ON tab_situacoes.id_situacao = tab_cobricoes.id_situacao  
+                        LEFT JOIN tab_toques ON tab_toques.id_cobricao_relacionada = tab_cobricoes.id_cobricao  
+                        LEFT JOIN tab_situacoes AS tab_situacao_toque ON tab_situacao_toque.id_situacao = tab_toques.id_situacao_prenhez  
+                        LEFT JOIN tab_sexagens ON tab_sexagens.id_cobricao_relacionada = tab_cobricoes.id_cobricao  
+                        LEFT JOIN tab_situacoes AS tab_situacao_sexagens ON tab_situacao_sexagens.id_situacao = tab_sexagens.id_resultado_sexagem  
+                        LEFT JOIN tab_comunicacoes_cobricao_associacao ON tab_comunicacoes_cobricao_associacao.id_cobricao_relacionada = tab_cobricoes.id_cobricao  
+                        LEFT JOIN tab_animais AS tab_receptora_comunicacao ON tab_receptora_comunicacao.id_animal = tab_comunicacoes_cobricao_associacao.id_receptora_comunicacao 
+                        LEFT JOIN tab_nascimentos ON tab_nascimentos.id_cobricao = tab_cobricoes.id_cobricao
+                    WHERE
+                        
+                        tab_cobricoes.id_usuario_sistema = :ID_PROPRIETARIO AND
+                        tab_cobricoes.id_situacao = '1' AND
+                        tab_comunicacoes_cobricao_associacao.protocolo_comunicacao IS NULL AND
+                        tab_toques.id_situacao_prenhez = 19
+                    GROUP BY tab_cobricoes.id_cobricao
+                    ORDER BY tab_cobricoes.data_cobertura ASC";
+
+            $pdo = $this->conn->conectar();
+            $res = $pdo->prepare($query_sql);
+            $res->bindValue(":ID_PROPRIETARIO", $id_proprietario);
+            $res->execute();
+
+            $dados = $res->fetchAll(PDO::FETCH_ASSOC);    
+            if (count($dados) <= 0) return erro("Nenhuma Cobrição foi localizada!");
+            
+            return sucesso("", ["dados"=> $dados]);
+        } catch (\Throwable $th) {
+            throw new Exception($th->getMessage(), (int)$th->getCode());
+        }
+        
+    }
 }
